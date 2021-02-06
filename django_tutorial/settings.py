86dae785c5e0cc06416e369d24fb6e89ef79a011
a18juliopc <a18juliopc@iessanclemente.net>
2021-02-07 00:52:38 +0100

    comit_0

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9f0h)gozf$g%6igo8&767w1xro0adm+)msxe)!eic$!fhvynb8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'prueba',
+	'USER': 'usuario',
+	'PASSWORD': 'abc123.',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
